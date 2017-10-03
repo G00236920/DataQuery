a7017a58a3bccc20a0c30157cb7c460e837ba912
NlTESHADE <nlteshade@Deboras-MacBook-Pro.local>
2017-10-03 23:39:21 +0100

    Added Menu loop

--- Problem 1/01HelloWorld.go
+++ Problem 1/01HelloWorld.go
@@ -8,8 +8,14 @@ import (
 
 func main() {
 
+	menu()
+
+}
+
+func menu() {
+
 	fmt.Println("This Program will say 'Hello World'  in either Japanese or English")
-	fmt.Print("Enter: \n1 for Japanese \n2 for English: \n")
+	fmt.Print("Enter: \n1. Japanese \n2. English: \n3. Exit \n")
 
 	var x int
 	fmt.Scan(&x)
@@ -18,6 +24,10 @@ func main() {
 		fmt.Println("こんにちは, 世界")
 	} else if x == 2 {
 		fmt.Println("Hello World")
+	} else {
+		return
 	}
+
+	menu()
 
 }
