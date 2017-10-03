fdbbaaac8a867967996574fc98bf12cd0c51d07c
NlTESHADE <nlteshade@Deboras-MacBook-Pro.local>
2017-10-03 23:29:26 +0100

    Input added for HelloWorld

--- Problem 1/01HelloWorld.go
+++ Problem 1/01HelloWorld.go
@@ -2,8 +2,22 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
-  fmt.Println("こんにちは, 世界")
+func main() {
+
+	fmt.Println("This Program will say 'Hello World'  in either Japanese or English")
+	fmt.Print("Enter: \n1 for Japanese \n2 for English: \n")
+
+	var x int
+	fmt.Scan(&x)
+
+	if x == 1 {
+		fmt.Println("こんにちは, 世界")
+	} else if x == 2 {
+		fmt.Println("Hello World")
+	}
+
 }
