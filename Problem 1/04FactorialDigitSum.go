b50ea77cdbe89e12ff498f53ea04e479e1c98609
NlTESHADE <nlteshade@Deboras-MacBook-Pro.local>
2017-10-03 23:56:45 +0100

    Menu Options Added

--- Problem 1/04FactorialDigitSum.go
+++ Problem 1/04FactorialDigitSum.go
@@ -8,6 +8,33 @@ import (
 
 func main() {
 
+	menu()
+
+}
+
+func menu() {
+
+	fmt.Println("Please Choose an Option")
+	fmt.Print("1. Find Factorial: \n")
+	fmt.Print("2. Exit: \n")
+
+	var input int
+	fmt.Scan(&input)
+
+	switch input {
+	case 1:
+		option1()
+	case 2:
+		return
+	default:
+		fmt.Println("Incorrect Entry")
+	}
+
+	menu()
+}
+
+func option1() {
+
 	total := new(big.Int)
 
 	fmt.Println("This Program will calculate the Factorial of any Integer Number Entered")
