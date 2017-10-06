985c0403187248c68961916621d76dbe962e48ac
NlTESHADE <nlteshade@Deboras-MacBook-Pro.local>
2017-10-06 21:19:17 +0100

    Square Root Comments

--- Problem 1/09SquareRoot.go
+++ Problem 1/09SquareRoot.go
@@ -14,23 +14,23 @@ func main() {
 func menu() {
 
 	var choice int
-	var entry float64
+	var entry float64 //float Variable
 
-	for i := 0; i < 1; {
+	for i := 0; i < 1; { //infinite loop for menu - thought id change it up
 
 		fmt.Printf("\nPlease Choose an Option\n")
 		fmt.Println("\t1. Find the Square root of a value")
 		fmt.Println("\t2. Exit")
 		fmt.Println()
-		fmt.Scan(&choice)
+		fmt.Scan(&choice) // User input
 
 		switch choice {
 		case 1:
 			fmt.Println("Enter the Number you wish to find the square root of")
-			fmt.Scan(&entry)
+			fmt.Scan(&entry) //user input
 
 			Square := sqrt(entry)
-			fmt.Printf("\nThe square root of %.2f is %.4f\n", entry, Square)
+			fmt.Printf("\nThe square root of %.2f is %.4f\n", entry, Square) //Output the square root and original value
 		case 2:
 			return
 		default:
@@ -43,7 +43,7 @@ func menu() {
 func sqrt(x float64) float64 {
 
 	value := 1.0
-	minValue := 0.00000000001
+	minValue := 0.00000000001 //limit the amount the square root will trace down as far as
 	guess := 1.0
 
 	for i := 0; math.Abs(guess) > minValue; i++ {
@@ -53,6 +53,6 @@ func sqrt(x float64) float64 {
 
 	}
 
-	return value
+	return value // return the square root
 
 }
