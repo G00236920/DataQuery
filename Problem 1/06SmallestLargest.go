bee22607a904fd5dd53aeb81035ea8a41c5083d8
NlTESHADE <nlteshade@Deboras-MacBook-Pro.local>
2017-10-06 14:26:37 +0100

    Smallest Largest Comments

--- Problem 1/06SmallestLargest.go
+++ Problem 1/06SmallestLargest.go
@@ -2,39 +2,39 @@ package main
 
 import "fmt"
 
-type Node struct {
+type Node struct { //Struct called node
 	prev *Node
 	next *Node
 	key  string
 }
 
-type List struct {
+type List struct { //List double linked
 	head *Node
 	tail *Node
 }
 
-func (L *List) Insert(key string) {
+func (L *List) Insert(key string) { //add values to the node placed in a list
 	list := &Node{
 		next: L.head,
 		key:  key,
 	}
-	if L.head != nil {
+	if L.head != nil { //if the head value is null, then you are at the start of the list
 		L.head.prev = list
 	}
 	L.head = list
 
 	l := L.head
-	for l.next != nil {
+	for l.next != nil { //if you havent reached the end of the list
 		l = l.next
 	}
 	L.tail = l
 }
 
-func (l *List) Show() {
+func (l *List) Show() { //Display the list
 	fmt.Println()
 
 	list := l.head
-	for list != nil {
+	for list != nil { //if the list is not empty
 		fmt.Printf("%+v \n", list.key)
 		list = list.next
 	}
@@ -42,7 +42,7 @@ func (l *List) Show() {
 	fmt.Println()
 }
 
-func (l *List) find() {
+func (l *List) find() { //determine the smallest and largest values based on how many characters in the string
 
 	var smallest string
 	var largest string
@@ -50,17 +50,17 @@ func (l *List) find() {
 	list := l.head
 	for list != nil {
 
-		if len(smallest) == 0 {
+		if len(smallest) == 0 { // find smallest string
 			smallest = list.key
 		}
 
 		if len(list.key) < len(smallest) {
 			smallest = list.key
-		} else if len(list.key) > len(largest) {
+		} else if len(list.key) > len(largest) { //find largest string
 			largest = list.key
 		}
 
-		list = list.next
+		list = list.next //move list to next node
 	}
 	fmt.Println()
 	fmt.Println("The Smallest Element is: ", smallest)
@@ -74,12 +74,13 @@ func main() {
 
 }
 
-func menu() {
+func menu() { //Menu Function
 	l := List{}
 
 	var choice int
 	var entry string
 
+	//output information to the screen
 	for i := 0; i < 1; {
 		fmt.Println()
 		fmt.Println("Please Choose an Option")
@@ -93,17 +94,17 @@ func menu() {
 		switch choice {
 		case 1:
 			fmt.Println("Enter the Value for the Element")
-			fmt.Scan(&entry)
-			l.Insert(entry)
+			fmt.Scan(&entry) //take user input
+			l.Insert(entry)  //insert that input to the list
 		case 2:
-			l.find()
+			l.find() //find function
 		case 3:
-			l.Show()
+			l.Show() //display function
 		case 4:
 			return
 		default:
 			fmt.Println()
-			fmt.Println("INVALID OPTION")
+			fmt.Println("INVALID OPTION") //user input invalid info
 			fmt.Println()
 		}
 
