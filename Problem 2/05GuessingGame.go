591306df751625e02eeeb111377532eccc6df741
NlTESHADE <G00236920@gmit.ie>
2017-10-27 20:35:18 +0100

    Part 5 Complate

--- Problem 2/05GuessingGame.go
+++ Problem 2/05GuessingGame.go
@@ -1,11 +1,12 @@
 package main
 
 import (	
+	"time"
 	"fmt"												//Imports
 	"net/http"
 	"text/template"
 	"math/rand"
-	//"strconv"
+	"strconv"
 )
 
 type TemplateData struct {
@@ -13,18 +14,20 @@ type TemplateData struct {
 }
 
 func templateHandler(w http.ResponseWriter, r *http.Request) {	//Handle Http requests
-		
-		Target := 0 
-	
+
+
 		// Try to read the cookie.
 		var cookie, err = r.Cookie("Target")
 		
 		if err == nil {
 			// If we could read it, try to convert its value to an int.
 			if(Target == 0){
-
+				rand.Seed(time.Now().UnixNano())
 				Target = rand.Intn(19) + 1
-
+				fmt.Printf("\nRandom Number is: %s",strconv.Itoa(Target))
+			}else{
+				fmt.Printf("\nRandom Number is: %s",strconv.Itoa(Target)) 
+				
 			}
 		}
 	
@@ -38,16 +41,16 @@ func templateHandler(w http.ResponseWriter, r *http.Request) {	//Handle Http req
 
 		http.SetCookie(w, cookie)
 
-		fmt.Printf("\nRandom Number is: %d",Target) 
-
 	t, _ := template.ParseFiles("template/guess.html")											//Parse the template File
 	t.Execute(w, TemplateData{Message: "Guess a Number Between 1 and 20"})						// Execute the Tmpl file
 
 }
 
+var Target int = 0
 
 func main() {
 
+	fmt.Printf("\nRandom Number is: %s",strconv.Itoa(Target))
 	http.Handle("/", http.FileServer(http.Dir("./")))		//Handle http request
 	http.HandleFunc("/guess",templateHandler)				//handle requests for templates
 	
