b3ab59e3fc46fcb6f73228c4ad9ceeeac75e8fb1
NlTESHADE <nlteshade@MacbookPro.local>
2017-11-28 22:52:54 +0000

    Part 6 Started

---
+++ Problem 3/Part 6.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"strings"
+	"time"
+)
+
+//list of inputs
+var str = []string{
+	"People say I look like both my mother and father.",
+	"Father was a teacher.",
+	"I was my Father’s favourite.",
+	"I'm looking forward to the weekend.",
+	"My grandfather was French!",
+	"I am happy.",
+	"I am not happy with your responses.",
+	"I am not sure that you understand the effect that your questions are having on me.",
+	"I am supposed to just take what you're saying at face value?",
+	"I am tired of all the assignments",
+	"I am Looking forward to some time off",
+	"I am Hungry for chicken and waffles",
+}
+
+func main() {
+
+	for i := 0; i < 9; i++ {
+
+		//User Input strings
+		fmt.Println("intput: " + str[i] + "\n")
+
+		//Response
+		fmt.Println("output: " + ElizaResponse(str[i]) + "\n")
+
+	}
+
+}
+
+//Response function
+func ElizaResponse(input string) string {
+
+	re := regexp.MustCompile("(?i)" + `(?i)i\'?(?:\s?am|m)([^.?!]*)[.?!]?`) //Adapted for Part 4
+
+	if matched := re.MatchString(input); matched { //Compare question with the users input
+
+		return re.ReplaceAllString(Reflect(input), "How do you know you are $1??")
+
+	}
+
+	re2 := regexp.MustCompile("(?i)" + `\bfather\b`) //Read the input for the question
+
+	//If the input mentions Father
+	if matched := re2.MatchString(input); matched { //Compare question with the users input
+
+		return "Why don’t you tell me more about your father?"
+
+	}
+
+	//answer posibilities
+	answers := []string{
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
+		"How does that make you feel?",
+		"Why do you say that?",
+	}
+
+	//Seed time
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	//Return random answer
+	return answers[rand.Intn(len(answers))]
+
+}
+
+func Reflect(input string) string {
+
+	// Split the input on word boundaries.
+	boundaries := regexp.MustCompile("(?i)" + `\b`)
+	tokens := boundaries.Split(input, -1)
+
+	// List the reflections.
+	reflections := [][]string{
+		{`you're `, `i'm`},
+		{`your`, `my`},
+		{`you`, `I`},
+		{`me`, `you`},
+	}
+
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
+			}
+		}
+	}
+
+	// Put the tokens back together.
+	return strings.Join(tokens, ``)
+}
